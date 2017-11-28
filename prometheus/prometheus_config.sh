8e0ab647afb5dbff62d0ac41a5dd127c604e26e8
Vladimir S. Baranov <330034@gmail.com>
2017-11-28 23:25:34 +0600

    add files *-con for containers

--- prometheus/prometheus_config.sh
+++ prometheus/prometheus_config.sh
@@ -2,7 +2,7 @@
 
 #set -a
 #source ../.env-dev
-#/bin/bash -c "envsubst < /tmp/prometheus/prometheus.yml > /etc/prometheus/prometheus.yml"
+/bin/bash -c "envsubst < /tmp/prometheus/prometheus.yml > /etc/prometheus/prometheus.yml"
 /bin/bash -c "envsubst < /tmp/prometheus/alert.rules.yml > /etc/prometheus/alert.rules.yml"
 #/bin/bash -c "envsubst < /tmp/prometheus/prometheus.yml > tt.txt"
 
