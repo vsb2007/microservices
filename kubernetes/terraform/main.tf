1d7ffed2c315f4ea17d57fa1e908b202d42c4e8c
Vladimir S. Baranov <330034@gmail.com>
2017-12-13 15:27:05 +0600

    terraform fmt

--- kubernetes/terraform/main.tf
+++ kubernetes/terraform/main.tf
@@ -4,56 +4,25 @@ provider "google" {
   region      = "${var.region}"
 }
 
-###############
-
 resource "google_container_cluster" "primary" {
   name               = "gke-test"
   zone               = "europe-west1-b"
   initial_node_count = 2
 
-  #  additional_zones = [
-  #    "us-central1-b",
-  #    "us-central1-c",
-  #  ]
-
-  #  master_auth {
-  #    username = "mr.yoda"
-  #    password = "adoy.rm"
-  #  }
-  node_version = "1.8.3-gke.0"
+  node_version       = "1.8.3-gke.0"
   min_master_version = "1.8.3-gke.0"
+
   node_config {
-    #    oauth_scopes = [  #      "https://www.googleapis.com/auth/compute",  #      "https://www.googleapis.com/auth/devstorage.read_only",  #      "https://www.googleapis.com/auth/logging.write",  #      "https://www.googleapis.com/auth/monitoring",  #    ]
-
-    #    labels {  #      foo = "bar"  #    }
-
-    #    tags = ["foo", "bar"]
-
     disk_size_gb = "20"
     machine_type = "g1-small"
   }
 
   addons_config {
-    kubernetes_dashboard  {
+    kubernetes_dashboard {
       disabled = false
     }
   }
 }
-
-# The following outputs allow authentication and connectivity to the GKE Cluster.
-#output "client_certificate" {
-#  value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
-#}
-
-
-#output "client_key" {
-#  value = "${google_container_cluster.primary.master_auth.0.client_key}"
-#}
-
-
-#output "cluster_ca_certificate" {
-#  value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
-#}
 
 module "vpc" {
   source        = "modules/vpc"
